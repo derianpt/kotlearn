3baf9334e6b65016ef79e0eaae8c279eefee1214
Derian Tungka <balibone@gmail.com>
2018-10-20 13:00:20 +0800

    Add @kotlin timeline to news feed

--- app/src/main/java/edu/is3261/kotlearn/MainActivity.kt
+++ app/src/main/java/edu/is3261/kotlearn/MainActivity.kt
@@ -6,9 +6,16 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.twitter.sdk.android.core.Twitter
 import edu.is3261.kotlearn.fragments.NewsFeed.NewsFeedFragment
 import edu.is3261.kotlearn.fragments.Quiz.QuizLandingFragment
 import edu.is3261.kotlearn.fragments.SocialFeed.SocialFeedFragment
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.DefaultLogger
+import com.twitter.sdk.android.core.TwitterConfig
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -48,6 +55,17 @@ class MainActivity : AppCompatActivity() {
         toolbar.setIcon(R.drawable.basics)
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectorException)
+
+        // initialize twitter feed for later use! Takes CONSUMER_KEY and CONSUMER_SECRET defined in
+        // secrets.xml
+        val authConfig = TwitterAuthConfig(resources.getString(R.string.twitter_CONSUMER_KEY), resources.getString(R.string.twitter_CONSUMER_SECRET))
+        Log.d("mainActivity", "authConfig: ${authConfig.consumerKey} , ${authConfig.consumerSecret}")
+        val config = TwitterConfig.Builder(this)
+                .logger(DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(authConfig)
+                .debug(true)
+                .build()
+        Twitter.initialize(config)
     }
 
     fun createSocialFeedFragment() {
--- app/src/main/java/edu/is3261/kotlearn/fragments/NewsFeed/NewsFeedFragment.kt
+++ app/src/main/java/edu/is3261/kotlearn/fragments/NewsFeed/NewsFeedFragment.kt
@@ -1,20 +1,25 @@
 package edu.is3261.kotlearn.fragments.NewsFeed
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.widget.SwipeRefreshLayout
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.twitter.sdk.android.tweetui.TweetTimelineRecyclerViewAdapter
+import com.twitter.sdk.android.tweetui.UserTimeline
 
 import edu.is3261.kotlearn.R
-import edu.is3261.kotlearn.feed.RedditFeed
 
 class NewsFeedFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var userTimeline: UserTimeline
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -26,19 +31,46 @@ class NewsFeedFragment : Fragment() {
 
         Toast.makeText(context,getString(R.string.social_feed_placeholder), Toast.LENGTH_SHORT).show()
 
-        // populate the feed by 1) downloading data from reddit and 2) populating swipeContainer
-        // pass in context for toasting errors
-//        RedditFeed(this.activity!!.applicationContext, view).execute()
-        //TODO: create twitter feed
+        // init user timeline
+        userTimeline = UserTimeline.Builder().screenName("kotlin").build()
+
+        // present data on view
+        initTwitterFeed(view)
 
         // Setup refresh listener which triggers new data loading
         swipeContainer.setOnRefreshListener {
             Toast.makeText(context,getString(R.string.social_feed_placeholder), Toast.LENGTH_SHORT).show()
             // This method performs the actual data-refresh operation.
-//            RedditFeed(this.activity!!.applicationContext, view).execute()
-            //TODO: replace with twitter feed
+            initTwitterFeed(view)
         }
 
         return view
     }
+
+    private fun initTwitterFeed(view: View){
+        viewManager = LinearLayoutManager(context)
+        viewAdapter = TweetTimelineRecyclerViewAdapter.Builder(this.context)
+                .setTimeline(userTimeline)
+                .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
+                .build();
+//       viewAdapter = TweetTimelineListAdapter.Builder(this.context)
+//               .setTimeline(userTimeline)
+//               .setViewStyle(R.style.tw__TweetLightWithActionsStyle)
+//               .build()
+        recyclerView = view.findViewById<RecyclerView>(R.id.news_recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
+        view.findViewById<SwipeRefreshLayout>(R.id.news_swipe_refresh).isRefreshing = false
+        Toast.makeText(context, context?.getString(R.string.social_feed_done), Toast.LENGTH_SHORT).show()
+    }
 }
+
